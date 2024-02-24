ba24b395b29b224ce6cd07eccb98fc12aa54e836
FATHI MAHDI ELSIDDIG HAROUN <43938127+FathiMahdi@users.noreply.github.com>
2024-02-24 12:54:42 +0800

    modified:   main.py

--- Src/main.py
+++ Src/main.py
@@ -1,1 +1,14 @@
-# import lib
+import sys
+from main_parameter_handler import parseMainParameter
+
+
+# initialize system parameter list
+system_parameters = []
+
+# get system parameters
+for arg in sys.argv:
+    system_parameters.append(arg)
+
+
+# parse system parameter        
+parseMainParameter(system_parameters,len(system_parameters))
