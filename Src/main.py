f0ef132d5e4d45d419ef048f0f54bd086ea1c9f9
FATHI MAHDI ELSIDDIG HAROUN <43938127+FathiMahdi@users.noreply.github.com>
2024-03-02 15:08:56 +0800

    :wq 	modified:   ../assets (modified content, untracked content)

--- Src/main.py
+++ Src/main.py
@@ -2,54 +2,33 @@ import sys
 from main_parameter_handler import *
 import argparse
 
-
-
-
-
-
 def main():
 
     # show program banner
     banner('EXFY TOOL')
 
-    parser = argparse.ArgumentParser(description='EXFY')
-    parser.add_argument('--yara','-y',type=str,help='check rules ',metavar='file path or dir path')
+
+    # get the user flags
+    parser = argparse.ArgumentParser(prog='EXFY', description='A tool that compine yara and exiftool',epilog='-h or --help to see all commands')
+    parser.add_argument('--yara','-y',help='check rules ',action='store_true')
     parser.add_argument('--custom',type=str,help='run costum rules',metavar='yara rules file')
-    parser.add_argument('--scan','-s',type=str,help='yara scan with repository rules',metavar='file path or dir path')
-    parser.add_argument('--update','-u',type=str,help='update yara rules',metavar='')
-    parser.add_argument('--exiftool','-e',type=str,help='exif tool',metavar='file path or dir path')
-    parser.add_argument('--csv','-c',type=str,help='export as csv',metavar='csv file name and path')
-    parser.add_argument('--report','-r',type=str,help='export yara html report',metavar='report name file name and path')
+    parser.add_argument('--dir',type=str,help='directory path',metavar='dir apth')
+    parser.add_argument('--file',type=str,help='file path',metavar='file path')
+    parser.add_argument('--repo',help='yara scan with repository rules',action='store_true')
+    parser.add_argument('--update','-u',help='update yara rules',action='store_true')
+    parser.add_argument('--exiftool','-e',help='exif tool',metavar='file path or dir path',type=str)
+    parser.add_argument('--csv','-c',type=str,help='export as csv',metavar='csv file name and path',required=False,default='exiftool_report.csv')
+    parser.add_argument('--report','-r',type=str,help='export yara html report',metavar='report name file name and path',default='YaraScanner_Report.html')
     parser.add_argument('--all','-a',type=str,help='Yara and exiftool',metavar='')
 
+
+
+    # construct argparser object
     args = parser.parse_args()
 
 
-
-
-    # initialize system parameter list
-    system_parameters = []
-
-    # get system parameters
-    for arg in sys.argv:
-        system_parameters.append(arg)
-
-    # check if all the flags are ok
-    detect, flag_error_index = FlagChecker(system_parameters)
-
-    # if all ok
-    if detect:
-        # parse system parameter        
-        parseMainParameter(system_parameters,len(system_parameters))
-
-    # else
-    else:
-        if len(system_parameters)==1:
-            print('add -h or --help to to see all commands')
-
-        elif len(system_parameters)>=2:
-            print('Error flag {} not found'.format(system_parameters[flag_error_index]))
-
+    #call parser handler
+    parserHandler(args)
 
 
 if __name__ == "__main__":
--- Src/main_parameter_handler.py
+++ Src/main_parameter_handler.py
@@ -4,113 +4,79 @@ from yara_handler import *
 from all_handler import *
 import yara_updater
 import yara_scanner
+import report_generator
+import common_functions
 
 
-# -h --help help and banner
-# -a -all exiftool + yara + all csv
-# -e --exiftool exiftool only
-# -y --yara  yara only
-# if only application name make -h
 
+def parserHandler(args):
 
-# PEY -e 3439yeqhr.png --csv kabgfkjbaf.csv
-# PEY -y  rule  path 
+    # parser yara rules update
+    if(args.update):
+        print("- Updating yara rules ...")
+        yara_updater.update()
+       
+    # parse exif tool
+    if(args.exiftool):
 
-avaiable_flags=[
-    '-e',
-    '--exiftool',
-    '-y',
-    '--yara',
-    '-h',
-    '--help',
-    '--csv',
-    '-c',
-    '--source',
-    '--update',
-    '-u',
-    '-s',
-    '--all',
-    '-a'
-]
+        if(args.csv):
 
+            try:
+                exifConvertToCsv(args.exiftool,args.csv)
+            except:
+                print('Please check the file/s path')
 
-def FlagChecker(system_parameters):
-
-    detect = False
-    flag_error_index = 0
-
-    for i in range(len(system_parameters)):
-
-        if system_parameters[i][0] == '-':
-
-            detect = False
-            for j in range(len(avaiable_flags)):
-
-                if system_parameters[i]==avaiable_flags[j]:
-                    detect = True
-
-            if detect== False:
-                flag_error_index = i
-                break   
-
-    return detect , flag_error_index
-
-
-def parseMainParameter(parameter,arg_counter):
-
-    if arg_counter > 1:
-
-        # for help
-        if parameter[1] == '--help' or parameter[1] == '-h':
-
-            print('Help')
-            return True
-        
-        # chek if exiftool is required
-        elif parameter[1] == '--exiftool' or parameter[1]== '-e':
-            
-            # check if csv extraction required
-            if (arg_counter>=4) and ((parameter[3] == '--csv') or (parameter[3] == '-c')):
-
-                exifConvertToCsv(parameter[2],parameter[4])
-
-            # others just extract the metadata ond show them in terminal
-            elif (arg_counter<=3):
-                exifExtractAll(parameter[2])
-
-        # chek if yara is required
-        elif parameter[1] == '--yara' or parameter[1]== '-y': 
-            
-            # PEY -y  -s rule  path 
-            if (arg_counter==5) and ((parameter[2] == '--source') or (parameter[2] == '-s')):
-                yaraRunRule(parameter[3],parameter[4])
-     
-            # others just extract the metadata ond show them in terminal
-            elif (arg_counter==4) and ((parameter[2] != '--source') or (parameter[2] != '-s')):
-              
-                yaraRunRuleFromFile(parameter[2],parameter[3])    
-
-        # chek if all is required
-        elif parameter[1] == '--all' or parameter[1]== '-a':
-
-            # check if csv extraction required
-            if (arg_counter>=4) :
-
-                if arg_counter==5:
-                  
-                    ExportALLToCsv(parameter[2],parameter[3],parameter[4])
-                    
-                elif arg_counter==4:
-                    ExportALLToCsv(parameter[2],parameter[3])
-
-
-        # no command found
         else:
-        
-            return False
-        
-    else :
+            try:
+                exifExtractAll(args.exiftool)
+            except:
+                print('Please check the file/s path')
 
-        # TODO: print help
-        return True
-    
+
+    # parse yara
+    if(args.yara):
+        
+        # run yara with custom rule
+        if(args.custom):
+
+            try:
+                if(args.file):
+                    
+                    yaraRunRule(args.custom,args.file)
+                    
+
+                elif(args.dir):
+
+                    yaraRunRuleFromFile(args.custom,args.dir)
+            except:
+                print('Erorr running yara on custom rules')
+
+        
+        # run yara with repository rules
+        elif(args.repo):
+
+            try:
+                if(args.file):
+                    match_result = yara_scanner.scan_file(args.file)
+
+                elif(args.dir):
+                    match_result = yara_scanner.scan_directory(args.dir, 1)
+
+
+                if match_result is None:
+                    raise Exception()
+            except:
+                sys.exit(0)
+
+        if(args.report):
+            report = report_generator.generate_report(match_result)
+            common_functions.write_to_file(args.report, report)
+            print('[+] Report saved to "{}"'.format(args.report))
+
+
+
+
+
+    # parse all
+           
+ #   exfy --yara --file/dir file/dir --custom dd --repo --report
